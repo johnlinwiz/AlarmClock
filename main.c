e0832e4bfe1bad09a609fa56515c451365523af5
Sean Wernimont <wernimont86@hotmail.com>
2018-08-30 22:07:24 -0500

    added rtc6 example for reference

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "mcc_generated_files/mcc.h"
 #include "oled.h"
 #include "clock.h"
-//#include "mcc_generated_files/RTC6.h"
+#include "mcc_generated_files/RTC6.h"
 
 void SetupClock();
+void rtc_Example();
 
 void main(void)
 {
@@ -22,18 +23,52 @@ void main(void)
     getTime = 0; //Time in Seconds   
     char timeStr[];
     OLED_Clear();
-    while(1){
-        getTime = clock_GetTime();
+    rtc_Example();
+    /*while(1){
+        //getTime = clock_GetTime();
+        getTime = rtc6_GetTime();
         tm_t = localtime(&getTime);
         sprintf(timeStr, "%02d:%02d:%02d\n", tm_t->tm_hour, tm_t->tm_min, tm_t->tm_sec);
         Write_String(timeStr);
         __delay_ms(100);
+    }*/
+}
+void rtc_Example(){
+    volatile time_t getTime, setTime;
+
+    struct tm *tm_t;
+
+    char buffer[80];
+
+    memset(tm_t, 0, sizeof (tm_t));
+
+    /*** Variable Initializations ***/
+    setTime = 1503870020; //Time in Seconds
+    getTime = 0; //Time in Seconds   
+    char timeStr[];
+    while (1) {
+        /** Example to Set Time to RTCC */
+        rtc6_SetTime(setTime);
+        __delay_ms(20);
+
+        while (1) {
+            /** Example to Get Time from RTCC */
+            getTime = rtc6_GetTime();
+            tm_t = localtime(&getTime);
+            sprintf(timeStr, "%04d-%02d-%02d %02d:%02d:%02d\n", tm_t->tm_year+1900, tm_t->tm_mon+1, tm_t->tm_mday, tm_t->tm_hour, tm_t->tm_min, tm_t->tm_sec);
+            Write_String(timeStr);
+            __delay_ms(1000);
+        }
     }
 }
 
 void SetupClock(){
+    volatile time_t setTime;
+    setTime = 1503870020; //Time in Seconds
+    //rtc6_SetTime(setTime);
+    rtc6_SetTime2(1999, 8, 30, 9, 56);
     //clock_SetTime(1999, 8, 30, 9, 56);
     //clock_SetHour(10);
     //clock_SetMinute(13);
-    __delay_ms(1000);
+    __delay_ms(20);
 }
