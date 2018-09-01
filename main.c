0b7830c7fb63a4efe8e15f0d8f693e7b01c09844
Sean Wernimont <wernimont86@hotmail.com>
2018-09-01 16:42:10 -0500

    correctly keeping time.

--- main.c
+++ main.c
@@ -47,15 +47,12 @@ void rtc_Example(){
     getTime = 0; //Time in Seconds   
     char timeStr[];
     while (1) {
-        /** Example to Set Time to RTCC */
-        rtc6_SetTime(setTime);
-        __delay_ms(20);
-
+        //rtc6_SetTime2(1999, 8, 30, 9, 56);
+        //__delay_ms(20);
         while (1) {
-            /** Example to Get Time from RTCC */
             getTime = rtc6_GetTime();
             tm_t = localtime(&getTime);
-            sprintf(timeStr, "%04d-%02d-%02d %02d:%02d:%02d\n", tm_t->tm_year+1900, tm_t->tm_mon+1, tm_t->tm_mday, tm_t->tm_hour, tm_t->tm_min, tm_t->tm_sec);
+            sprintf(timeStr, "%02d:%02d:%02d\n", tm_t->tm_hour, tm_t->tm_min, tm_t->tm_sec);
             Write_String(timeStr);
             __delay_ms(1000);
         }
@@ -66,7 +63,7 @@ void SetupClock(){
     volatile time_t setTime;
     setTime = 1503870020; //Time in Seconds
     //rtc6_SetTime(setTime);
-    rtc6_SetTime2(1999, 8, 30, 9, 56);
+    rtc6_SetTime2(1999, 8, 30, 5, 12);
     //clock_SetTime(1999, 8, 30, 9, 56);
     //clock_SetHour(10);
     //clock_SetMinute(13);
