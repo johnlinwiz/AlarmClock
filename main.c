0dea6e2ffcd26b4d14696a1d39cc59ef5ec1d4f3
Sean Wernimont <wernimont86@hotmail.com>
2018-09-02 17:27:36 -0500

    clean up.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "RTC6.h"
 
 void SetupClock();
-void rtc_Example();
+void pollClock();
 
 void main(void)
 {
@@ -18,30 +18,26 @@ void main(void)
     INTERRUPT_PeripheralInterruptEnable();
     
     OLED_Clear();
-    rtc_Example();
-}
-void rtc_Example(){
-    volatile time_t getTime, setTime;
-
-    struct tm *tm_t;
-
-
-    memset(tm_t, 0, sizeof (tm_t));
-
-    /*** Variable Initializations ***/
-    setTime = 1503870020; //Time in Seconds
-    getTime = 0; //Time in Seconds   
-    char timeStr[];
-    while (1) {
-        getTime = rtc6_GetTime();
-        tm_t = localtime(&getTime);
-        sprintf(timeStr, "%02d:%02d:%02d\n", tm_t->tm_hour, tm_t->tm_min, tm_t->tm_sec);
-        Write_String(timeStr);
-        __delay_ms(10);
+    while(1){
+        pollClock();
     }
 }
 
 void SetupClock(){
-  //  rtc6_SetTime2(1999, 8, 30, 10, 2);
+    rtc6_SetTime2(1999, 8, 30, 14, 1);
     __delay_ms(20);
+}
+
+void pollClock(){
+    volatile time_t getTime, setTime;
+    struct tm *tm_t;
+    memset(tm_t, 0, sizeof (tm_t));
+    setTime = 1503870020; //Time in Seconds
+    getTime = 0; //Time in Seconds   
+    char timeStr[];
+    getTime = rtc6_GetTime();
+    tm_t = localtime(&getTime);
+    sprintf(timeStr, "%02d:%02d:%02d\n", tm_t->tm_hour, tm_t->tm_min, tm_t->tm_sec);
+    Write_String(timeStr);
+    __delay_ms(10);
 }
